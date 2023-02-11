9b627b102f36c2eb76025e477725a11d364664ab
Navin Aananthan K R <81963819+NavinAananthan@users.noreply.github.com>
2023-02-11 11:58:16 +0530

    Update Algo.py

--- Market Hotness/Algo.py
+++ Market Hotness/Algo.py
@@ -1,30 +1,23 @@
 import pandas as pd
 import streamlit as st
-import numpy as np
-from matplotlib import pyplot as plt
+
+
+def get_avg_price(market,market_metrics,market_id):
+    avg_prices = market_metrics.groupby('market_id')['median_sale_price'].mean()
+    avg_prices=pd.DataFrame(avg_prices)
+    m_id=market_id['market_id']
+    avg_prices['m_id']=sorted(m_id)
+
+    st.write("Visualization of Average Prices with respect to their Market id")
+    st.line_chart(avg_prices)
+    st.write("\n We can interpret that from the above given")
+
 
 
 market=pd.read_csv("E:\Zero-Down Hackathon\Market Hotness\market.csv")
 market_metrics=pd.read_csv("E:\Zero-Down Hackathon\Market Hotness\market_metrics.csv")
-
-
-avg_prices = market_metrics.groupby('market_id')['median_sale_price'].mean()
-avg_prices=pd.DataFrame(avg_prices)
-
 market_id=market_metrics.groupby('market_id').count().reset_index()
 
-m_id=market_id['market_id']
-
-avg_prices['m_id']=sorted(m_id)
-
-#print(max(avg_prices['median_sale_price']))
+get_avg_price(market,market_metrics,market_id)
 
 
-st.line_chart(avg_prices)
-
-
-#plt.plot(avg_prices['m_id'],avg_prices['median_sale_price'])
-#plt.show()
-
-#print(market.head())
-#print(market_metrics.head())
